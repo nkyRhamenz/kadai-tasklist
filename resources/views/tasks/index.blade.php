b8c737625856e5465bec7cad8cbd91835f5c877d
toshihiko.nakaya <nkyrhamenz@gmail.com>
2019-01-28 04:02:19 +0000

    validation and add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,6 +9,7 @@
             <thead>
                 <tr>
                     <th>id</th>
+                    <th>ステータス</<th>
                     <th>タスク</th>
                 </tr>
             </thead>
@@ -16,6 +17,7 @@
                 @foreach ($tasks as $task)
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
@@ -23,6 +25,6 @@
         </table>
     @endif
     
-    {!! link_to_route('tasks.create', '新規タスクの投稿',[], ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', '新規タスクの作成',[], ['class' => 'btn btn-primary']) !!}
     
 @endsection
